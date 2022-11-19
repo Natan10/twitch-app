2654e4c93070f7f38cdf65226b327b3c92a937c5
Natan moreira <natan-moreira1@hotmail.com>
2022-11-19 08:55:31 -0300

    add id to user

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -3,6 +3,7 @@ import { Alert } from 'react-native';
 import * as AuthSession from 'expo-auth-session';
 
 interface User {
+  id: number;
   email: string;
   name: string;
   photo?: string;
@@ -36,13 +37,6 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
       const authUrl = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${redirectUrl}&scope=user%3Aread%3Aemail`;
       const {type, params} = await AuthSession.startAsync({authUrl}) as AuthorizationResponse;
       
-
-      // login: narutinhoDoNordeste
-      // senha: @Saradao89
-
-      // https://api.twitch.tv/helix/games/top
-      // https://api.twitch.tv/helix/users
-      console.log(params)
       if(type === 'success') {
         const response = await fetch(`https://api.twitch.tv/helix/users`,{
           headers: {
@@ -54,6 +48,7 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
         const objUser = data.data[0];
 
         setUser({
+          id: Number(objUser.id),
           email: objUser.email,
           name: objUser.display_name,
           photo: objUser.profile_image_url
@@ -66,7 +61,7 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
       Alert.alert('Erro ao conectar a conta Twitch')
     }
   }
-
+  
   return(
     <AuthContext.Provider value={{user, signIn}}>
       {children}
